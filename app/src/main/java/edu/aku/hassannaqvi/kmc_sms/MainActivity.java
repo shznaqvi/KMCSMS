69b0aaaec5ca43524b6981d64a320a5b57048b60
hassan.naqvi <shznaqvi@gmail.com>
2019-08-01 11:31:11 +0500

    Initial Commit

---
+++ app/src/main/java/edu/aku/hassannaqvi/kmc_sms/MainActivity.java
@@ -0,0 +1,119 @@
+package edu.aku.hassannaqvi.kmc_sms;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements MessageListener {
+
+    private static final String TAG = "Main Activity";
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 1;
+
+    Button smsButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Register sms listener
+        //MessageReceiver.bindListener(this);
+        smsButton = findViewById(R.id.smsButton);
+        checkForSmsPermission();
+    }
+
+    @Override
+    public void messageReceived(String message) {
+        Toast.makeText(this, "New Message Received: " + message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
+                if (permissions[0].equalsIgnoreCase(Manifest.permission.SEND_SMS)
+                        && grantResults[0] ==
+                        PackageManager.PERMISSION_GRANTED) {
+                    enableSmsButton();
+
+                    // Permission was granted.
+                } else {
+                    // Permission denied.
+                    Log.d(TAG, getString(R.string.failure_permission));
+                    Toast.makeText(MainActivity.this,
+                            getString(R.string.failure_permission),
+                            Toast.LENGTH_SHORT).show();
+                    // Disable the message button.
+                    disableSmsButton();
+                }
+            }
+        }
+    }
+
+    private void checkForSmsPermission() {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS) !=
+                PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, getString(R.string.permission_not_granted));
+            // Permission not yet granted. Use requestPermissions().
+            // MY_PERMISSIONS_REQUEST_SEND_SMS is an
+            // app-defined int constant. The callback method gets the
+            // result of the request.
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.SEND_SMS},
+                    MY_PERMISSIONS_REQUEST_SEND_SMS);
+        } else {
+            // Permission already granted. Enable the message button.
+            enableSmsButton();
+        }
+    }
+
+    private void enableSmsButton() {
+        smsButton.setEnabled(true);
+        smsButton.setText("SEND SMS");
+    }
+
+    private void disableSmsButton() {
+        smsButton.setEnabled(false);
+        smsButton.setText("");
+
+    }
+
+    public void smsSendMessage(View view) {
+        // Find the TextView number_to_call and assign it to textView.
+        TextView textView = findViewById(R.id.number_to_call);
+        // Concatenate "smsto:" with phone number to create smsNumber.
+        String smsNumber = "smsto:" + textView.getText().toString();
+        // Find the sms_message view.
+        EditText smsEditText = findViewById(R.id.sms_message);
+        // Get the text of the sms message.
+        String sms = smsEditText.getText().toString();
+        // Create the intent.
+        Intent smsIntent = new Intent(Intent.ACTION_SENDTO);
+        // Set the data for the intent as the phone number.
+        smsIntent.setData(Uri.parse(smsNumber));
+        // Add the message (sms) with the key ("sms_body").
+        smsIntent.putExtra("sms_body", sms);
+        // If package resolves (target app installed), send intent.
+        if (smsIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(smsIntent);
+            Toast.makeText(this, "SMS Sent", Toast.LENGTH_SHORT).show();
+        } else {
+            Log.e(TAG, "Can't resolve app for ACTION_SENDTO Intent.");
+            Toast.makeText(this, "SMS NOT Sent", Toast.LENGTH_SHORT).show();
+
+
+        }
+    }
+
+}
---
+++ app/src/main/java/edu/aku/hassannaqvi/kmc_sms/MessageReceiver.java
@@ -0,0 +1,45 @@
+package edu.aku.hassannaqvi.kmc_sms;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
+
+public class MessageReceiver extends BroadcastReceiver {
+
+    private static MessageListener mListener;
+    private SharedPreferences preferences;
+
+    public static void bindListener(MessageListener listener) {
+        mListener = listener;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO Auto-generated method stub
+
+        if (intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")) {
+            Bundle bundle = intent.getExtras();           //---get the SMS message passed in---
+            SmsMessage[] msgs = null;
+            String msg_from;
+            if (bundle != null) {
+                //---retrieve the SMS message received---
+                try {
+                    Object[] pdus = (Object[]) bundle.get("pdus");
+                    msgs = new SmsMessage[pdus.length];
+                    for (int i = 0; i < msgs.length; i++) {
+                        msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                        msg_from = msgs[i].getOriginatingAddress();
+                        String msgBody = msgs[i].getMessageBody();
+                        //this.tv
+                    }
+                } catch (Exception e) {
+                    Log.d("Exception caught", e.getMessage());
+                }
+            }
+        }
+    }
+}
